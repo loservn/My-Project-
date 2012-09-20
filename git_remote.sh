94a6549d94bf4808e41930f190d8db5dae31b641
NgoHuy <huynhok.uit@gmail.com>
2012-09-20 23:42:45 +0700

    lazy boy

---
+++ git_remote.sh
@@ -0,0 +1,16 @@
+cd $(pwd)
+echo "you want to add or remove ? (a/r)"
+read ans
+echo "please enter your file you will commit"
+read file_name
+echo "you describe you file: "
+read -e describe
+if [ $ans = 'a' ]
+then
+ git add $file_name
+elif [ $ans = "r" ]
+then
+ git rm $file_name
+fi
+git commit -m "$describe"
+git push
