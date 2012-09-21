594bc297cd8a23ad96d9bc96629036b9b2c864ba
NgoHuy <huynhok.uit@gmail.com>
2012-09-21 17:58:40 +0700

    update git_remote

--- git_remote.sh
+++ git_remote.sh
@@ -3,6 +3,7 @@ echo "you want to add or remove ? (a/r)"
 read ans
 echo "please enter your file you will commit"
 read file_name
+shift
 echo "you describe you file: "
 read -e describe
 if [ $ans = 'a' ]
